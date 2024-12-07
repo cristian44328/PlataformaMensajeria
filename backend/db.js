b23abbe2f6671f5628b6ac661adbf210090ca7a9
cristian <cp1031716@gmail.com>
2024-12-06 23:05:37 -0400

    comienzo registro y autenticacion

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb://localhost/mensajeriadb");  
+        console.log(">>> DB conectada");
+    } catch (error) {   
+        console.log(error);
+    }
+};
